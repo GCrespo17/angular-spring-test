3cfac0ab16afb017f732a2cf3cc413b3fa454811
Gabriel <50721663+GCrespo17@users.noreply.github.com>
2024-11-10 12:17:06 -0400

    Se borro componente dummy

--- libreria-prueba-front/src/app/app.component.ts
+++ libreria-prueba-front/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { NavigationBarComponent } from "./navigation-bar/navigation-bar.component";
-import { PruebaComponent } from "./prueba/prueba.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavigationBarComponent, PruebaComponent],
+  imports: [RouterOutlet, HeaderComponent, NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
